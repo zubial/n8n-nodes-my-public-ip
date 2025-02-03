fcaccff26af6bcfbcc97f56b5544b49f0b7d03a5
brodrigues <rodrigues.bertrand@gmail.com>
2025-02-03 09:09:06 +0100

    Refactor MyPublicIp node to enhance flexibility and add options.
    Replaced static routing with dynamic HTTP requests for better extensibility, including IPv4, IPv6, and combined support. Added customizable output field options, improving usability and alignment with user requirements.

--- nodes/MyPublicIp/MyPublicIp.node.ts
+++ nodes/MyPublicIp/MyPublicIp.node.ts
@@ -1,4 +1,11 @@
-import { INodeType, INodeTypeDescription, NodeConnectionType } from 'n8n-workflow';
+import {
+	IExecuteFunctions,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+	NodeConnectionType
+} from 'n8n-workflow';
+import {IHttpRequestOptions} from "n8n-workflow/dist/Interfaces";
 
 export class MyPublicIp implements INodeType {
 	description: INodeTypeDescription = {
@@ -15,13 +22,6 @@ export class MyPublicIp implements INodeType {
 		},
 		inputs: [NodeConnectionType.Main],
 		outputs: [NodeConnectionType.Main],
-		requestDefaults: {
-			baseURL: '',
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-			},
-		},
 		properties: [
 			{
 				displayName: 'Use ipify.org to find your public IP address',
@@ -39,27 +39,77 @@ export class MyPublicIp implements INodeType {
 						name: 'Ip V4',
 						value: 'ip_v4',
 						action: 'Get my public ipv4',
-						routing: {
-							request: {
-								method: 'GET',
-								url: 'https://api.ipify.org?format=json',
-							},
-						},
 					},
 					{
 						name: 'Ip V6',
 						value: 'ip_v6',
 						action: 'Get my public ipv6',
-						routing: {
-							request: {
-								method: 'GET',
-								url: 'https://api6.ipify.org?format=json',
-							},
-						},
+					},
+					{
+						name: 'Both V4/V6',
+						value: 'both',
+						action: 'Get my public ipv4/ipv6',
 					},
 				],
 				default: 'ip_v4',
 			},
+			{
+				displayName: 'Options',
+				name: 'options',
+				type: 'collection',
+				placeholder: 'Add option',
+				default: {},
+				options: [
+					{
+						displayName: 'Put Result in Field',
+						name: 'result_field',
+						type: 'string',
+						default: 'public_ip',
+						description: 'The name of the output field to put the data in',
+					},
+				],
+
+			},
 		],
 	};
+
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const items = this.getInputData();
+
+		const version = this.getNodeParameter('version', 0);
+		const options = this.getNodeParameter('options', 0);
+		const result_field = (options.result_field ? options.result_field as string : 'public_ip');
+
+		let httpOptions: IHttpRequestOptions = {
+			method: 'GET',
+			url: '',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+		};
+
+		let response: { ip: string | number | boolean | object | null | undefined; };
+		if (version == 'both' || version == 'ip_v6') {
+			httpOptions.url = 'https://api6.ipify.org?format=json';
+
+			response = await this.helpers.httpRequest(httpOptions);
+			if (response.ip) {
+				items.forEach((item) => (item.json[result_field + ((version == 'both') ? '_v6':'')] = response.ip));
+			}
+			console.log(response);
+		}
+
+		if (version == 'both' || version == 'ip_v4') {
+			httpOptions.url = 'https://api.ipify.org?format=json';
+
+			response = await this.helpers.httpRequest(httpOptions);
+			if (response.ip) {
+				items.forEach((item) => (item.json[result_field + ((version == 'both') ? '_v4':'')] = response.ip));
+			}
+			console.log(response);
+		}
+
+		return [items];
+	}
 }
